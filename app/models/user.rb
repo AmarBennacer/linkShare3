f97bf20549752dfc3733dafb37753db8425616aa
aminebennace2020 <aminebennace2020@gmail.com>
2023-03-23 17:50:34 +0100

    fixed sign in issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
 
          has_many :links
-
+         acts_as_voter
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :links
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-#root to: "links#index"
-#root to: 'home#index'
-root 'links#index'
+ resources :links do
+  member do
+    put 'like' => 'links#like'
+  end
+end  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'links#index'
 end
